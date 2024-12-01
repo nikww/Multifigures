e80b99254b35af936b9e45d72f6323170166a72d
Admin <Admin@DESKTOP-OQUHL3P>
2024-12-01 23:37:29 +0300

    Добавьте файлы проекта.

---
+++ Multifigures/FIgures/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multifigures.FIgures
+{
+    public abstract class Shape
+    {
+        internal int x, y, r;
+        internal bool IsInside = false;
+    }
+}
---
+++ Multifigures/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Multifigures
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
